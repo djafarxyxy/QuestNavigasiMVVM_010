f014ced4af5a1df9938cebabf31f7ddbbfd19d31
djafarxyxy <farhanch12344@gmail.com>
2024-12-17 16:17:48 +0700

    menambahkan body untuk detail mhs

--- app/src/main/java/com/example/tugas9/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/tugas9/ui/view/mahasiswa/DetailMhsView.kt
@@ -127,3 +127,64 @@ fun ItemDetailMhs(
     }
 }
 
+@Composable
+fun BodyDetailMhs(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailMhsUiState = DetailMhsUiState(),
+    onDeleteClick: () -> Unit = { }
+) {
+    var deleteConfirmationReuired by rememberSaveable { mutableStateOf(false) }
+    when {
+        detailUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty -> {
+            Column (
+                modifier = modifier.fillMaxWidth()
+                    .padding(16.dp)
+            ){
+                ItemDetailMhs(
+                    mahasiswa = detailUiState.detailUiEvent.toMahasiswaEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = { deleteConfirmationReuired = true },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+
+                if (deleteConfirmationReuired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationReuired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = { deleteConfirmationReuired = false },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+
+        detailUiState.isUiEventEmpty -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data Tidak Ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
