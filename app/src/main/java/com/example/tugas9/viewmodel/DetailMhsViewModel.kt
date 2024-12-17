5c1c80cd8e821e14811c5d009ddd201b57c88926
djafarxyxy <farhanch12344@gmail.com>
2024-12-17 16:37:27 +0700

    mengelola data dan status UI pada halaman detail mahasiswa, serta mengonversi data mahasiswa

--- app/src/main/java/com/example/tugas9/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/tugas9/viewmodel/DetailMhsViewModel.kt
@@ -59,3 +59,24 @@ class DetailMhsViewModel(
         }
     }
 }
+data class DetailMhsUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get()= detailUiEvent == MahasiswaEvent()
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
+}
+fun Mahasiswa.toDetailUiEvent(): MahasiswaEvent {
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
+}
